03b1378517bf5e450110ec8350532350422b724e
songyeonhee <y_y970125@naver.com>
2026-07-27 00:20:02 +0900

    a0726_1

---
+++ Java/Hello/src/homework/Homework.java
@@ -0,0 +1,21 @@
+package homework;
+
+// Java 연습 문제 (2026-07-24) 2-2 (코드 작성)
+// {85, 90, 78, 92, 88}` 배열의 **최대값**을 찾아 출력하는 프로그램을 작성하세요.
+
+public class Homework {
+    public static void main(String[] args) {
+       int[] num1 = {85, 90, 78, 92, 88};
+       int max = 0;
+
+       for (int i=0; i<num1.length; i++) {
+        int result = num1[i];
+        if(result > max) {
+            max = result;
+        }
+       }
+        System.out.print("max : " + max);
+    }
+}
+
+    
